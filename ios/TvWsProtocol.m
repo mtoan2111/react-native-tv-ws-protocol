60442a68c4998ef5c5d1a6387577baebdfb4abb2
Toàn Nguyễn Mạnh <toan93.hust@gmail.com>
2020-10-26 10:47:07 +0700

    change to swift

--- ios/TvWsProtocol.m
+++ ios/TvWsProtocol.m
@@ -1,16 +1,9 @@
-//
-//  TvWsProtocol.m
-//  TvWsProtocol
-//
-//  Created by Nguyễn Mạnh Toàn on 10/24/20.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(TvWsProtocol, NSObject)
 
-RCT_EXTERN_METHOD(create:(NSString *)uri options:(NSDictionary *)options)
+RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
 
 @end
-
